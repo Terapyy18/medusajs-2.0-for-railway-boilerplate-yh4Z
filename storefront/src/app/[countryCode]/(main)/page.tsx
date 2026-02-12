db617dc464f6eea06093db66eb070262b36d3605
Theo Dumontet <144424403+Terapyy18@users.noreply.github.com>
2026-02-12 22:52:03 +0100

    recommanded procuts home page

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -1,14 +1,12 @@
 import { Metadata } from "next"
 
-import FeaturedProducts from "@modules/home/components/featured-products"
-
 import Hero from "@modules/home/components/hero"
 import Craftsmanship from "@modules/home/components/craftsmanship"
 import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 import Newsletter from "@modules/home/components/newsletter"
-
+import RecommendedProducts from "@modules/home/components/recommended-products"
 
 export const metadata: Metadata = {
   title: "TeraPrint Studio",
@@ -35,9 +33,7 @@ export default async function Home({
       <Hero />
 
       <ul className="flex flex-col gap-x-6">
-        {collections && region && (
-          <FeaturedProducts collections={collections} region={region} countryCode={countryCode} />
-        )}
+        <RecommendedProducts countryCode={countryCode} />
       </ul>
       <Craftsmanship countryCode={countryCode} />
       <Newsletter />
