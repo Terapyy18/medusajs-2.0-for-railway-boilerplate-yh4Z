f995eb6aa3a710ae10e6ee200faaa8caf7914f93
Theo Dumontet <144424403+Terapyy18@users.noreply.github.com>
2026-03-15 17:42:14 +0100

    ajout commande et reglage nbugs

--- storefront/src/modules/collections/templates/index.tsx
+++ storefront/src/modules/collections/templates/index.tsx
@@ -5,6 +5,7 @@ import RefinementList from "@modules/store/components/refinement-list"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 import PaginatedProducts from "@modules/store/templates/paginated-products"
 import { HttpTypes } from "@medusajs/types"
+import { getDictionary } from "@lib/dictionary"
 
 export default function CollectionTemplate({
   sortBy,
@@ -19,10 +20,11 @@ export default function CollectionTemplate({
 }) {
   const pageNumber = page ? parseInt(page) : 1
   const sort = sortBy || "created_at"
+  const dictionary = getDictionary(countryCode).store
 
   return (
     <div className="flex flex-col small:flex-row small:items-start py-6 content-container">
-      <RefinementList sortBy={sort} />
+      <RefinementList sortBy={sort} dictionary={dictionary} />
       <div className="w-full">
         <div className="mb-8 text-2xl-semi">
           <h1>{collection.title}</h1>
